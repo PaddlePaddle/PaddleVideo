b55b5139d3417d2fd91be299cac9fa27e1c38144
chajchaj <306536853@qq.com>
2020-12-01 04:39:04 +0000

    add tweaks resnet in backbones for pptsm

---
+++ run_pptsm.sh
@@ -0,0 +1,3 @@
+export CUDA_VISIBLE_DEVICES=4,5,6,7
+python3.7 -B -m paddle.distributed.launch --log_dir=log_tsm.pptsm  main.py --parallel --validate -c configs/recognition/tsm/pptsm.yaml -o log_level="INFO" 
+
