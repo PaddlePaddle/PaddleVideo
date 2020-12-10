13db84f873d72214614452d87f4d9aaed4f23c75
chajchaj <306536853@qq.com>
2020-12-10 04:03:26 +0000

    use r50vdssld in paddle2.0 and change config of precisebn

--- run_pptsm.sh
+++ run_pptsm.sh
@@ -1,3 +1,8 @@
-export CUDA_VISIBLE_DEVICES=4,5,6,7
-python3.7 -B -m paddle.distributed.launch --log_dir=log_tsm.pptsm  main.py  --validate -c configs/recognition/tsm/pptsm.yaml -o log_level="INFO" 
+CURRENT=`date "+%Y-%m-%d %H:%M:%S"`
+echo "start time: $CURRENT"
 
+export CUDA_VISIBLE_DEVICES=4,5,6,7
+python3.7 -B -m paddle.distributed.launch --log_dir=log_pptsm  main.py  --validate -c configs/recognition/tsm/pptsm.yaml -o log_level="INFO" 
+
+CURRENT=`date "+%Y-%m-%d %H:%M:%S"`
+echo "end time: $CURRENT"
