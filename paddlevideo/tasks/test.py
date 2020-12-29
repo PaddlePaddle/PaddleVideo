f4addc9563f320b80f9838075511ec5a0873a27f
shippingwang <shipeng1108@163.com>
2020-12-29 11:54:48 +0000

    fix

--- paddlevideo/tasks/test.py
+++ paddlevideo/tasks/test.py
@@ -28,7 +28,7 @@ def test_model(model, dataset, cfg, weight, world_size):
         dataset (paddle.dataset): Train dataaset.
     """
     #NOTE: add a new field : test_batch_size ?
-    batch_size = cfg.DATASET.get("batch_size", 2)
+    batch_size = cfg.DATASET.get("test_batch_size", 2)
 
     places = paddle.set_device('gpu')
 
