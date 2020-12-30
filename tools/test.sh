da3965e542f06c799ffa64bce3bbd91c41de3262
huangjun12 <12272008@bjtu.edu.cn>
2020-12-30 08:52:32 +0000

    fix slowfast bug, add test_batch_size in config file

--- tools/test.sh
+++ tools/test.sh
@@ -6,4 +6,4 @@ python3 -B -m paddle.distributed.launch --gpus="0,1,2,3" --log_dir=log_test tool
 #python3.7 -B -m paddle.distributed.launch --selected_gpus="0,1,2,3,4,5,6,7" --log_dir=logs_test test.py  -c ../configs/recognition/slowfast/slowfast.yaml -w "../output/SlowFast/SlowFast_epoch_00196.pdparams"
 
 # run bmn test, only support single card, bs=1
-#python3.7 test.py -c ../configs/localization/bmn.yaml -w ../output/BMN/BMN_epoch_00001.pdparams  -o DATASET.batch_size=1
+#python3.7 test.py -c ../configs/localization/bmn.yaml -w ../output/BMN/BMN_epoch_00010.pdparams  -o DATASET.batch_size=1
