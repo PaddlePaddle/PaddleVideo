2a1167979a1f33996d372e207b76bc4df39629b8
mohui37 <77723348+mohui37@users.noreply.github.com>
2021-01-20 18:46:42 +0800

    Update paddlevideo_clas.py

--- tools/paddlevideo_clas.py
+++ tools/paddlevideo_clas.py
@@ -291,7 +291,7 @@ class PaddleVideo(object):
         total_result = []
         for filename in video_list:
             if isinstance(filename, str):
-                v = utils.decode(video, self.args)
+                v = utils.decode(filename, self.args)  #2021.1.20_mohui37
                 assert v is not None, "Error in loading video: {}".format(
                     filename)
                 inputs = utils.preprocess(v, self.args)
