f82b2e6d9f867f930818c088c45ae334ac7d25a2
xt <2761564455@qq.com>
2021-02-04 15:33:57 +0800

    del modality in Normalization

--- paddlevideo/loader/pipelines/augmentations.py
+++ paddlevideo/loader/pipelines/augmentations.py
@@ -1157,46 +1157,46 @@ class Normalize:
     def __call__(self, results):
         #modality = results['modality']
 
-        if modality == 'RGB':
-            n = len(results['imgs'])
-            h, w, c = results['imgs'][0].shape
-            imgs = np.empty((n, h, w, c), dtype=np.float32)
-            for i, img in enumerate(results['imgs']):
-                imgs[i] = img
+        # if modality == 'RGB':
+        n = len(results['imgs'])
+        h, w, c = results['imgs'][0].shape
+        imgs = np.empty((n, h, w, c), dtype=np.float32)
+        for i, img in enumerate(results['imgs']):
+            imgs[i] = img
 
-            for img in imgs:
-                imnormalize_(img, self.mean, self.std, self.to_bgr)
+        for img in imgs:
+            imnormalize_(img, self.mean, self.std, self.to_bgr)
 
-            results['imgs'] = imgs
-            results['img_norm_cfg'] = dict(
-                mean=self.mean, std=self.std, to_bgr=self.to_bgr)
-            return results
-        if modality == 'Flow':
-            num_imgs = len(results['imgs'])
-            assert num_imgs % 2 == 0
-            assert self.mean.shape[0] == 2
-            assert self.std.shape[0] == 2
-            n = num_imgs // 2
-            h, w = results['imgs'][0].shape
-            x_flow = np.empty((n, h, w), dtype=np.float32)
-            y_flow = np.empty((n, h, w), dtype=np.float32)
-            for i in range(n):
-                x_flow[i] = results['imgs'][2 * i]
-                y_flow[i] = results['imgs'][2 * i + 1]
-            x_flow = (x_flow - self.mean[0]) / self.std[0]
-            y_flow = (y_flow - self.mean[1]) / self.std[1]
-            if self.adjust_magnitude:
-                x_flow = x_flow * results['scale_factor'][0]
-                y_flow = y_flow * results['scale_factor'][1]
-            imgs = np.stack([x_flow, y_flow], axis=-1)
-            results['imgs'] = imgs
-            args = dict(
-                mean=self.mean,
-                std=self.std,
-                to_bgr=self.to_bgr,
-                adjust_magnitude=self.adjust_magnitude)
-            results['img_norm_cfg'] = args
-            return results
+        results['imgs'] = imgs
+        results['img_norm_cfg'] = dict(
+            mean=self.mean, std=self.std, to_bgr=self.to_bgr)
+        return results
+        # if modality == 'Flow':
+        #     num_imgs = len(results['imgs'])
+        #     assert num_imgs % 2 == 0
+        #     assert self.mean.shape[0] == 2
+        #     assert self.std.shape[0] == 2
+        #     n = num_imgs // 2
+        #     h, w = results['imgs'][0].shape
+        #     x_flow = np.empty((n, h, w), dtype=np.float32)
+        #     y_flow = np.empty((n, h, w), dtype=np.float32)
+        #     for i in range(n):
+        #         x_flow[i] = results['imgs'][2 * i]
+        #         y_flow[i] = results['imgs'][2 * i + 1]
+        #     x_flow = (x_flow - self.mean[0]) / self.std[0]
+        #     y_flow = (y_flow - self.mean[1]) / self.std[1]
+        #     if self.adjust_magnitude:
+        #         x_flow = x_flow * results['scale_factor'][0]
+        #         y_flow = y_flow * results['scale_factor'][1]
+        #     imgs = np.stack([x_flow, y_flow], axis=-1)
+        #     results['imgs'] = imgs
+        #     args = dict(
+        #         mean=self.mean,
+        #         std=self.std,
+        #         to_bgr=self.to_bgr,
+        #         adjust_magnitude=self.adjust_magnitude)
+        #     results['img_norm_cfg'] = args
+        #     return results
         raise NotImplementedError
 
     def __repr__(self):
