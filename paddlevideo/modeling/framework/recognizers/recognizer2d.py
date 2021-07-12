86ea981a63d4f52b8a90649dd092234b110fc3e1
HydrogenSulfate <490868991@qq.com>
2021-07-12 20:50:33 +0800

    Update recognizer2d.py

--- paddlevideo/modeling/framework/recognizers/recognizer2d.py
+++ paddlevideo/modeling/framework/recognizers/recognizer2d.py
@@ -23,10 +23,8 @@ class Recognizer2D(BaseRecognizer):
     """2D recognizer model framework."""
     def forward_net(self, imgs):
         # NOTE: As the num_segs is an attribute of dataset phase, and didn't pass to build_head phase, should obtain it from imgs(paddle.Tensor) now, then call self.head method.
-        if self.head_name != "TimeSformerHead":
-            num_segs = imgs.shape[
-                1]  # imgs.shape=[N,T,C,H,W], for most commonly case
-            imgs = paddle.reshape_(imgs, [-1] + list(imgs.shape[2:]))
+        num_segs = imgs.shape[1]  # imgs.shape=[N,T,C,H,W], for most commonly case
+        imgs = paddle.reshape_(imgs, [-1] + list(imgs.shape[2:]))
 
         if self.backbone != None:
             feature = self.backbone(imgs)
@@ -34,10 +32,7 @@ class Recognizer2D(BaseRecognizer):
             feature = imgs
 
         if self.head != None:
-            if self.head_name == "TimeSformerHead":
-                cls_score = self.head(feature)
-            else:
-                cls_score = self.head(feature, num_segs)
+            cls_score = self.head(feature, num_segs)
         else:
             cls_score = None
 
@@ -62,18 +57,8 @@ class Recognizer2D(BaseRecognizer):
     def test_step(self, data_batch):
         """Define how the model is going to test, from input to output."""
         # NOTE: (shipping) when testing, the net won't call head.loss, we deal with the test processing in /paddlevideo/metrics
-        if self.head_name == 'TimeSformerHead':  # [N,C,Tx3,H,W], for uniform_crop case
-            clips_list = paddle.split(
-                data_batch[0], num_or_sections=3,
-                axis=2)  # [N, 3, T, H, W], [N, 3, T, H, W], [N, 3, T, H, W]
-            cls_score = [
-                paddle.nn.functional.softmax(self.forward_net(imgs), axis=1)
-                for imgs in clips_list
-            ]  # [N, C], [N, C], [N, C]
-            cls_score = sum(cls_score)  # [N, C] in [0,1]
-        else:
-            imgs = data_batch[0]
-            cls_score = self.forward_net(imgs)
+        imgs = data_batch[0]
+        cls_score = self.forward_net(imgs)
         return cls_score
 
     def infer_step(self, data_batch):
