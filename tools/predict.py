7b677e989a7153c044839daed9570340c2704d1e
HydrogenSulfate <490868991@qq.com>
2021-12-01 22:18:14 +0800

    Update install.md in test_tipc, and fix some bug

--- tools/predict.py
+++ tools/predict.py
@@ -99,6 +99,7 @@ def create_paddle_predictor(args, cfg):
     config.enable_memory_optim()
     # use zero copy
     config.switch_use_feed_fetch_ops(False)
+    config.delete_pass("shuffle_channel_detect_pass")
     predictor = create_predictor(config)
 
     return config, predictor
