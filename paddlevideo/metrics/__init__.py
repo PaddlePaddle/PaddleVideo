5653c242aab0c766002d8daceb106b0951b17b1d
lxz203000 <55575535+lxz203000@users.noreply.github.com>
2022-01-06 19:00:53 +0800

    Update __init__.py

--- paddlevideo/metrics/__init__.py
+++ paddlevideo/metrics/__init__.py
@@ -22,10 +22,11 @@ from .registry import METRIC
 from .skeleton_metric import SkeletonMetric
 from .transnetv2_metric import TransNetV2Metric
 from .youtube8m.eval_util import HitOneMetric
+from .vos_metric import VOSMetric
 from .center_crop_metric_MRI import CenterCropMetric_MRI
 
 __all__ = [
     'METRIC', 'build_metric', 'MultiCropMetric', 'BMNMetric',
     'CenterCropMetric', 'SkeletonMetric', 'HitOneMetric', 'TransNetV2Metric',
-    'DepthMetric', 'MSRVTTMetric', 'CenterCropMetric_MRI'
+    'DepthMetric', 'MSRVTTMetric', 'VOSMetric', 'CenterCropMetric_MRI'
 ]
