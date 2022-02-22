0fc9812076f5824d8b9575560b48b88d41bd59e9
huangjun12 <12272008@bjtu.edu.cn>
2022-02-22 15:12:29 +0000

    fix styl

--- paddlevideo/modeling/heads/movinet_head.py
+++ paddlevideo/modeling/heads/movinet_head.py
@@ -9,17 +9,8 @@ from ..builder import build_loss
 @HEADS.register()
 class MoViNetHead(BaseHead):
 
-    def __init__(
-        self,
-        #  num_classes,
-        #  in_channels,
-        # loss_cfg=dict(name="CrossEntropyLoss")
-    ):
+    def __init__(self):
         super().__init__()
-        #super().__init__(num_classes, in_channels, loss_cfg)
-        # self.num_classes = num_classes
-        # self.in_channels = in_channels
-        #self.loss_func = build_loss(loss_cfg)
 
     def forward(self, x):
         return x
