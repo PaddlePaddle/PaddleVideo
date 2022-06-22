aa0365f0452e74867484bfc83e89ebe0520b9d29
Wuxiao85 <36037443+Wuxiao85@users.noreply.github.com>
2022-06-22 15:18:14 +0800

    Delete applications directory

--- applications/EIVideo/QEIVideo/gui/__init__.py
+++
@@ -1,4 +0,0 @@
-# Author: Acer Zhang
-# Datetime: 2022/1/6 
-# Copyright belongs to the author.
-# Please indicate the source for reprinting.
--- applications/T2VLAD/logger/__init__.py
+++
@@ -1,2 +0,0 @@
-from .logger import *
-from .log_parser import *
