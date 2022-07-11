796f8c42ed7d425b10c5cc433d993a241d589f7a
ttjygbtj <chentju@qq.com>
2022-07-11 14:12:19 +0800

    提交

--- applications/BILS/frontend/.github/actions/get-build-number/index.js
+++
@@ -1,9 +0,0 @@
-async function main(output) {
-   output('build_number', process.env.GITHUB_RUN_NUMBER);
-}
-
-function setOutput(name, value) {
-    process.stdout.write(Buffer.from(`::set-output name=${name}::${value}`))
-}
-
-main(setOutput);
