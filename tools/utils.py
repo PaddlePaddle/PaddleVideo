e022da2e909d467fc6c7fc0caf59db81fa36ae83
zwtu <1252983925@qq.com>
2022-07-19 15:44:27 +0800

    Update

--- tools/utils.py
+++ tools/utils.py
@@ -270,7 +270,7 @@ class ppTSN_Inference_helper(Base_Inference_helper):
         img_mean = [0.485, 0.456, 0.406]
         img_std = [0.229, 0.224, 0.225]
         ops = [
-            VideoDecoder(backend="decord"),
+            VideoDecoder(),
             Sampler(self.num_seg,
                     self.seg_len,
                     valid_mode=True,
@@ -836,7 +836,6 @@ class AGCN2s_Inference_helper(Base_Inference_helper):
             input_file)
         data = np.load(input_file)
         results = {'data': data}
-        
 
         res = np.expand_dims(results['data'], axis=0).copy()
         return [res]
