62295d54b4ada9068406e1cdd4d47ce5784a0630
huangjun12 <12272008@bjtu.edn.cn>
2022-08-04 08:00:59 +0000

    remove shell

--- export.sh
+++
@@ -1,3 +0,0 @@
-python3.7 tools/export_model.py -c configs/recognition/movinet/movinet_k400_frame.yaml \
-                                -p data/MoViNetA0_k400.pdparams \
-                                -o inference/MoViNetA0
